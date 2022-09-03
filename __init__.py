d7807a7e2850fafcd30701ebedc980066585279c
enoTak <eno.sleepy.zzz.zz.z@gmail.com>
2022-09-03 22:43:28 +0900

    [impl] added numerical differenctial

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 from .core_simple import Variable
 from .core_simple import Function
 from .core_simple import Square, Exp
+from .core_simple import numerical_diff
--- core_simple.py
+++ core_simple.py
@@ -25,3 +25,11 @@ class Square(Function):
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
+
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)

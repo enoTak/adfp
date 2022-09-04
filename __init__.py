a621d55cf8315a11cc7336aefcefb8b3f03529f4
enoTak <eno.sleepy.zzz.zz.z@gmail.com>
2022-09-04 15:58:37 +0900

    [refactor] modified use of Functions to natuaal function style

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from .core_simple import Variable
 from .core_simple import Function
-from .core_simple import Square, Exp
 from .core_simple import numerical_diff
+
+from .functions import square, exp
---
+++ functions.py
@@ -0,0 +1,31 @@
+
+import numpy as np
+from .core_simple import Variable, Function
+
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+
+def square(x):
+    return Square()(x)
+
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+
+
+def exp(x):
+    return Exp()(x)

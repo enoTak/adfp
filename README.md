0d6aaa20905a8900f87eb7e56ee0c0747aface1e
enoTak <eno.sleepy.zzz.zz.z@gmail.com>
2022-09-12 09:43:47 +0900

    [docs] updated README and the desctiption in setup.py

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# numeric_ad
+# adfp
+'adfp' (Auto DiFferential for Python) is the framework for auto diffenretials.
+This project imtates the implemetation of latest deep learning framework.
--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup
+from codecs import open
+from os import path
+
+
 from adfp import __version__
+
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='adfp',
       version=__version__,
       license='MIT License',
       install_requires=['numpy'],
-      description='auto differential framework',
       author='Takumi Enomoto',
       author_email='eno.sleepy.zzz.zz.z@gmail.com',
       url='',
       packages=['adfp'],
+      description='auto differential framework',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      keywords='autodiffential',
       )

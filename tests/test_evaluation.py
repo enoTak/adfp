dfbb1e465b6ea6df1330ac3ecd0a2936273ee53a
enoTak <eno.sleepy.zzz.zz.z@gmail.com>
2022-09-18 17:49:54 +0900

    [test] added test for mse

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -28,3 +28,13 @@ class MeanSquareErrorTest(unittest.TestCase):
         actual = x1.grad
         expected = Variable(np.array([.5, -.5, 0., 1.]))
         self.assertEqual(actual, expected)
+
+    def test_matrix(self):
+        # division number = len of 1st tensor, not num of total elements
+        x0 = Variable(np.array([[1., 2., 3.], [3., 4., 5,]]))
+        x1 = Variable(np.array([[2., 1., 3.], [3., 6., 5.]]))
+        y = mean_square_error(x0, x1)
+
+        actual = y
+        expected = Variable(np.array(3.))
+        self.assertEqual(actual, expected)
